ceb0c8153ea08745071b5b6aaa530a9178fdc4ed
raphael.gicquiaux@gmail.com <raphael.gicquiaux@gmail.com@4d8ff6c5-92dd-aa1f-ae6e-84b804048d21>
2013-11-20 16:34:10 +0000

    Correctifs depuis la mise à jour git => SVN

--- trunk/src/com/ensibs/omeca/fragment/HandFragment.java
+++ trunk/src/com/ensibs/omeca/fragment/HandFragment.java
@@ -14,6 +14,6 @@ public class HandFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
         Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.view_fragment_hand, container, false);
+        return inflater.inflate(R.layout.view_hand, container, false);
     }
 }
