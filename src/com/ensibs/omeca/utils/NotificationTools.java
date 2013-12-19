47ed8713f1d5f42891804bd373f9002c82b35313
56.Rio.Sylvain <56.Rio.Sylvain@gmail.com>
2013-12-19 09:47:19 +0000

    Ajout de la classe utilitaire pour les notifications

---
+++ src/com/ensibs/omeca/utils/NotificationTools.java
@@ -0,0 +1,53 @@
+package com.ensibs.omeca.utils;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
+public class NotificationTools {
+	
+	/**
+	 * Id and counter of notification
+	 */
+	private static int counter = 1000;
+	
+	//TODO: return to the activity
+	/**
+	 * Display a notification in the notification bar
+	 * @param context application context
+	 * @param icon Drawable icon
+	 * @param title Title of the notification
+	 * @param content Content of the notification
+	 * @return Id of the new notification
+	 */
+	public static int createBarNotification(Context context, int icon, String title, String content){
+		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
+		mBuilder.setSmallIcon(icon);
+		mBuilder.setContentTitle(title);
+		mBuilder.setContentText(content);
+		NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		notificationManager.notify(counter,mBuilder.build());
+		counter ++;
+		return counter-1;
+	}
+
+	public static void createVibrationNotification(Context context, int time){
+		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
+		mBuilder.setVibrate(new long[]{100,time});
+		NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		notificationManager.notify(counter,mBuilder.build());
+		counter ++;
+	}
+	
+	public static void createSoundNotification(Context context){
+		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
+		Uri ringURI = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+		mBuilder.setSound(ringURI);
+		NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		notificationManager.notify(counter,mBuilder.build());
+		counter ++;
+	}
+	
+}
