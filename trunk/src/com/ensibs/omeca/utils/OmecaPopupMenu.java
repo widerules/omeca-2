9a2947337558157d7d86423bfba795d4fe4adde9
56.Rio.Sylvain@gmail.com <56.Rio.Sylvain@gmail.com@4d8ff6c5-92dd-aa1f-ae6e-84b804048d21>
2014-01-06 08:34:34 +0000

    Correction PopUpMenu : statique reference à une activity ==> cause des erreurs si l'activity se ferme.

--- trunk/src/com/ensibs/omeca/utils/OmecaPopupMenu.java
+++ trunk/src/com/ensibs/omeca/utils/OmecaPopupMenu.java
@@ -1,16 +1,17 @@
 package com.ensibs.omeca.utils;
 
-import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.view.LayoutInflater;
+
 import com.ensibs.omeca.R;
 
 public class OmecaPopupMenu {
 	private static AlertDialog omecaPopupMenu = null;
 
-	public static void show(Activity act) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(act);
-		LayoutInflater inflater = act.getLayoutInflater();
+	public static void show(Context context) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
 		omecaPopupMenu = builder.create();
 		omecaPopupMenu.setTitle("Options");
 		omecaPopupMenu.setView(inflater.inflate(R.layout.popup_menu, null));
