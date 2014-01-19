3bd62d3cc805cb9939210b49dcbb54c212dc11a0
lindsayrozier <lindsayrozier@gmail.com>
2014-01-19 15:01:26 +0000

    Organiser les cartes de la mains manuellement

--- src/com/ensibs/omeca/view/CardView.java
+++ src/com/ensibs/omeca/view/CardView.java
@@ -3,11 +3,14 @@ package com.ensibs.omeca.view;
 import android.content.ClipData;
 import android.content.Context;
 import android.util.DisplayMetrics;
+import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.ensibs.omeca.ControllerView;
 import com.ensibs.omeca.model.entities.Card;
 
 public class CardView extends ImageView{
@@ -30,46 +33,22 @@ public class CardView extends ImageView{
 		int width = (int) (height/RATIO);
 		setLayoutParams(new RelativeLayout.LayoutParams(width, height));
 		setOnTouchListener(new CardTouchListener());
+		setOnDragListener(null);
 				
 	}
-	public void onHand(boolean hand){ 
-		 if (hand = true)
-			 this.setOnTouchListener(new CardTouchListenerHand());
-		 else 
+	/*public void onHand(boolean hand){ 
+		 if (hand == true){
+			this.setOnTouchListener(new CardTouchListenerHand());
+		 	this.setOnDragListener(new OnDragListenerHand());
+		 	//Toast.makeText(context,"hand", Toast.LENGTH_SHORT).show();
+		 }else {
 			 this.setOnTouchListener(new CardTouchListener());
-	}
-	private class CardTouchListenerHand implements OnTouchListener{
-		private float y;
-		private final float SCROLL_THRESHOLD = 30 ;
-		private boolean isOnClick;
-		
-		@Override
-		public boolean onTouch(View view, MotionEvent mE) {
-			switch (mE.getAction()){
-				case MotionEvent.ACTION_DOWN:
-					y = mE.getY();
-					isOnClick = true;
-					break;
-				case MotionEvent.ACTION_MOVE:
-					if(isOnClick && ( y - mE.getY()) > SCROLL_THRESHOLD){
-						view.setVisibility(View.INVISIBLE);
-						ClipData data = ClipData.newPlainText("", "");
-				        DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
-				        view.startDrag(data, shadowBuilder, view, 0);
-				        isOnClick = false;
-					}
-					break;
-				case MotionEvent.ACTION_UP:
-					if(!isOnClick){
-						view.setVisibility(View.VISIBLE);
-					}
-					break;
-				default:
-					break;
-			}	
-			return true;
-		}
-	}
+		 	this.setOnDragListener(null);
+		 }
+	}*/
+	
+
+
 	
 	public void turnCard(){
 		if(card.isFaceUp())
--- src/com/ensibs/omeca/view/HandViewCardGalleryDragListener.java
+++ src/com/ensibs/omeca/view/HandViewCardGalleryDragListener.java
@@ -1,19 +1,28 @@
 package com.ensibs.omeca.view;
 
+import android.support.v4.view.MotionEventCompat;
+import android.text.Layout;
 import android.view.DragEvent;
 import android.view.View;
 import android.view.View.OnDragListener;
+import android.view.ViewGroup;
+
+import com.ensibs.omeca.ControllerView;
 import com.ensibs.omeca.GameActivity;
 import com.ensibs.omeca.R;
+import com.ensibs.omeca.model.entities.Card;
 import com.ensibs.omeca.utils.SlidingUpPanelLayout;
 
 public class HandViewCardGalleryDragListener implements OnDragListener{
 	boolean exited = false;
+	int width; 
 	@Override
     public boolean onDrag(View v, DragEvent event) {
+		final float initX = event.getX(); 
+		final float initY = event.getY();
 		switch (event.getAction()) {
 			case DragEvent.ACTION_DRAG_ENTERED:
-				exited = false;
+				exited = false;			     
 				break;
 			case DragEvent.ACTION_DRAG_EXITED:
 				SlidingUpPanelLayout slide = (SlidingUpPanelLayout) GameActivity.getActivity().findViewById(R.id.sliding_layout);
@@ -28,13 +37,21 @@ public class HandViewCardGalleryDragListener implements OnDragListener{
 				HandView hv =(HandView) view.getParent();
 				if(exited){
 					hv.removeView(view);
-				}
+				}/*}else if( event.getX() - (hv.getHeight()/2)== Math.abs(hv.getHeight()/3) ){ // ordonner une carte
+					int position = (int) (event.getY()/width);
+				
+					ControllerView.user.getCards().remove(view.getCard());
+					ControllerView.user.getCards().add(position+ hv.getFirstVisiblePosition(), view.getCard());
+				}*/
 				hv.updateView();
 				break;
 			default:
 				break;
 		}
-		
 		return true;
     }
+	
+	public void setWidth(int w){
+		this.width = w; 
+	}
 }
