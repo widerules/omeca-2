d4000604eeca86b579b7d187a5e552e968906ed0
raphael.gicquiaux <raphael.gicquiaux@gmail.com>
2014-02-04 10:53:57 +0000

    Menu quitter optimationnal

---
+++ src/com/ensibs/omeca/utils/OmecaPopupExit.java
@@ -0,0 +1,55 @@
+package com.ensibs.omeca.utils;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+import com.ensibs.omeca.GameActivity;
+import com.ensibs.omeca.MainActivity;
+
+public class OmecaPopupExit {
+	private static AlertDialog omecaPopupMenu = null;
+	private static Boolean isGameActivity = false;
+	private static String finishText = "Voulez-vous vraiment quitter l'application?";
+
+	public static void show(Context context) {
+		if (context instanceof	GameActivity) {
+			isGameActivity = true;
+			finishText = "Voulez-vous vraiment quitter la partie?";
+		}
+
+		DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				switch (which){
+				case DialogInterface.BUTTON_POSITIVE:
+					if (isGameActivity) {
+						GameActivity.getActivity().finishGameActivity();
+					} else {
+						MainActivity.getActivity().finishMainActivity();
+					}
+					break;
+
+				case DialogInterface.BUTTON_NEGATIVE:
+					omecaPopupMenu.cancel();
+					break;
+				}
+			}
+		};
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		omecaPopupMenu = builder.create();
+
+		builder
+		.setMessage(finishText)
+		.setPositiveButton("Oui", dialogClickListener)
+		.setNegativeButton("Non", dialogClickListener)
+		.show();
+
+	}
+
+	public static void dismiss() {
+		omecaPopupMenu.cancel();
+	}
+
+}
