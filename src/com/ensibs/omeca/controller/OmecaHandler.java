b9c8bd97c09a28acd9bfe71e8a0800775bc21400
56.Rio.Sylvain <56.Rio.Sylvain@gmail.com>
2014-02-08 13:57:36 +0000

    Retournement de carte dans la drawpile

--- src/com/ensibs/omeca/controller/OmecaHandler.java
+++ src/com/ensibs/omeca/controller/OmecaHandler.java
@@ -1,5 +1,6 @@
 package com.ensibs.omeca.controller;
 
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
@@ -9,12 +10,15 @@ import android.widget.Toast;
 import com.ensibs.omeca.GameActivity;
 import com.ensibs.omeca.R;
 import com.ensibs.omeca.view.BoardView;
+import com.ensibs.omeca.view.DrawPileView;
+import com.ensibs.omeca.wifidirect.property.WifiDirectProperty;
 
 public class OmecaHandler extends Handler{
 	
 	public static final int CONNEXION = 0;
 	public static final int DECONNEXION = 1;
 	public static final int AKNOWLEGMENT_CONNECTION_ACTION = 2;
+	public static final int RETURN_CARD = 3;
 	
 	public OmecaHandler(Looper looper){
 		super(looper);
@@ -36,6 +40,22 @@ public class OmecaHandler extends Handler{
 		else if(msg.what == AKNOWLEGMENT_CONNECTION_ACTION){
 			Log.w("deed", "deded");
 			((BoardView)GameActivity.getActivity().findViewById(R.id.view_board)).updatePlayers();
+		}
+		else if(msg.what == RETURN_CARD){
+			Log.i(WifiDirectProperty.TAG, "Retournement");
+			Bundle data = msg.getData();
+			BoardView boardView = (BoardView)GameActivity.getActivity().findViewById(R.id.view_board);
+			if(data.getString("Source").equals("DrawPileView")){
+				DrawPileView pileView = boardView.getDrawPileView();
+				//Log.i(WifiDirectProperty.TAG, pileView.getDrawpile().getCards().get(0).getValue()+" "+pileView.getDrawpile().getCards().get(0).getColor());
+				//Log.i(WifiDirectProperty.TAG, pileView.getDrawpile().getCards().get(pileView.getDrawpile().getNumberOfCards()-1).getValue()+" "+pileView.getDrawpile().getCards().get(pileView.getDrawpile().getNumberOfCards()-1).getColor());
+				pileView.getDrawpile().getCards().get(pileView.getDrawpile().getNumberOfCards()-1).setFaceUp(!pileView.getDrawpile().getCards().get(pileView.getDrawpile().getNumberOfCards()-1).isFaceUp());
+				pileView.updateView();
+			}else if(data.getString("Source").equals("BoardView")){
+				
+			}else if(data.getString("Source").equals("DiscardPileView")){
+				
+			}
 		}
 	}
 }
