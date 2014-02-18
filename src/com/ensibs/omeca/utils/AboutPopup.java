31f68c45689fc7b2aa00e86c6a083cf837a65f0c
raphael.gicquiaux <raphael.gicquiaux@gmail.com>
2014-02-18 09:55:29 +0000

    [CORRECTIF] Internationnalizatiooooooooooon powaaaaaaaaaaaaa

--- src/com/ensibs/omeca/utils/AboutPopup.java
+++ src/com/ensibs/omeca/utils/AboutPopup.java
@@ -1,6 +1,5 @@
 package com.ensibs.omeca.utils;
 
-import com.ensibs.omeca.GameActivity;
 import com.ensibs.omeca.R;
 
 import android.app.AlertDialog;
@@ -13,7 +12,7 @@ public class AboutPopup {
 		AlertDialog.Builder builder = new AlertDialog.Builder(context);
 
 		builder
-			.setMessage(GameActivity.getActivity().getResources().getString(R.string.about_text))
+			.setMessage(context.getResources().getString(R.string.about_text))
 			.show();
 
 	}
